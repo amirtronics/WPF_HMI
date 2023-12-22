a64ce7d49a303267c42d31f4379521dd28af17da
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-22 15:36:41 -0800

    Update FlangeWindow.xaml.cs

---
+++ HMI/HMI/View/FlangeWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace HMI.View
+{
+    /// <summary>
+    /// Interaction logic for FlangeWindow.xaml
+    /// </summary>
+    public partial class FlangeWindow : Window
+    {
+        public FlangeWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
