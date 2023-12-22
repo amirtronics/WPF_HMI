5788182a7b20315f23be2999d24e1a64fbbbd562
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-22 15:35:05 -0800

    Update MainWindow.xaml.cs

--- HMI/HMI/MainWindow.xaml.cs
+++ HMI/HMI/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using HMI.View;
 
 namespace HMI;
 
@@ -19,5 +11,68 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void btnMinimize_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void btnMaximize_Click(object sender, RoutedEventArgs e)
+    {
+        if (WindowState == WindowState.Maximized)
+            WindowState = WindowState.Normal;
+        else WindowState = WindowState.Maximized;
+    }
+
+    private void btnClose_Click(object sender, RoutedEventArgs e)
+    {
+        //Close();
+        Application.Current.Shutdown();
+    }
+
+    private void btnStart_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void btnStop_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void btnReset_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void btnHome_Click(object sender, RoutedEventArgs e)
+    {
+        var mainWindow = Application.Current.MainWindow;
+
+        // Iterate through all open windows and close them (excluding the current MainWindow)
+        foreach (Window window in Application.Current.Windows)
+        {
+            if (window != mainWindow)
+            {
+                window.Close();
+            }
+        }
+    }
+
+    private void btnFlange_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void btnProfile_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void btnCamera_Click(object sender, RoutedEventArgs e)
+    {
+        CameraWindow cameraWindow = new CameraWindow();
+        cameraWindow.Show();
     }
 }
