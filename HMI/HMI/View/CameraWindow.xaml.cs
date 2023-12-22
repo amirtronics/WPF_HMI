cd534cce2fff5ac4594df2786e10b4b2b749e906
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2023-12-22 15:36:38 -0800

    Update CameraWindow.xaml.cs

---
+++ HMI/HMI/View/CameraWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HMI.View
+{
+    /// <summary>
+    /// Interaction logic for CameraWindow.xaml
+    /// </summary>
+    public partial class CameraWindow : Window
+    {
+        public CameraWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            myWebBrowser.Navigate(SearchTermTextBox.Text);
+        }
+    }
+}
