9a70cd3640f24b9d3c798de2051ece51f088bbdd
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2024-01-04 14:19:56 -0800

    Create PLC.cs

---
+++ HMI/HMI/PLC.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using TwinCAT.Ads;
+
+namespace HMI
+{
+    public class PLC
+    {
+        TcAdsClient tcAds = new TcAdsClient();
+
+        public string adsAddress { get; set; }
+        public int adsPort { get; set; }
+
+        public void Connect()
+        {
+            tcAds.Connect(adsAddress, adsPort);
+        }
+
+        public void Disconnect()
+        {
+            tcAds.Disconnect();
+            tcAds.Dispose();
+        }
+    }
+}
