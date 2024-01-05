9cda2c1f553aaab7a289013c9822bd306e7f856b
Amir Ebrahimnezhad <amir.h.ebrahimnezhad@gmail.com>
2024-01-05 09:22:03 -0800

    Create Attempt.cs

---
+++ HMI/HMI/Attempt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HMI
+{
+    public class Attempt
+    {
+        [DllImport("attemptDll.dll")]
+        public static extern IntPtr Create(int x);
+
+        [DllImport("attemptDll.dll")]
+        public static extern int AttemptAdd(IntPtr a, int y);
+
+        public int AttemptFunc()
+        {
+            IntPtr a = Create(5);
+            int result = AttemptAdd(a, 10);
+            return result;
+        }
+    }
+}
